6b770804f3e54c757160af3859f7ada5159075b3
schubi <dario.schubert@gmail.com>
2018-05-03 13:41:44 +0200

    comitam

--- app/src/main/java/hr/vvg/kita/MainActivity.java
+++ app/src/main/java/hr/vvg/kita/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //kita u billa
         //kita u kita
+        //vidio promjenu
     }
 }
