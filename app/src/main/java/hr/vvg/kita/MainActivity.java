dc1e4526c3f83c91699dd4835d91c457f79b1cdd
picaboca <davorhiti@gmail.com>
2018-05-03 13:38:17 +0200

    jebi se

--- app/src/main/java/hr/vvg/kita/MainActivity.java
+++ app/src/main/java/hr/vvg/kita/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //kita u billa
+        //kita u kita
     }
 }
